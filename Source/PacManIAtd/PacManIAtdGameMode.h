a9b6119cc16d29d8d772400ec20aa7bb43df9949
Titouan Fréquelin <136023547+tfrequelt@users.noreply.github.com>
2024-10-09 09:25:43 -0400

    files add

---
+++ Source/PacManIAtd/PacManIAtdGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PacManIAtdGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APacManIAtdGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APacManIAtdGameMode();
+};
+
+
+
