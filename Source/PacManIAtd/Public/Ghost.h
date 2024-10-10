8e9fd57886674345fa9c96b81f2144af32c7eaa4
Titouan Fréquelin <136023547+tfrequelt@users.noreply.github.com>
2024-10-10 17:40:24 -0400

    add ghost

---
+++ Source/PacManIAtd/Public/Ghost.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "APacEntity.h"
+#include "Ghost.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PACMANIATD_API AGhost : public AAPacEntity
+{
+	GENERATED_BODY()
+	
+};
